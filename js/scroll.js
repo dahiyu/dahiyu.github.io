22f885cf4cccfba66d96f94a9f71d7c02e648ab5
Daichi Yamashita <yamashita_daichi@micard.co.jp>
2019-02-15 12:36:52 +0900

    ページ内リンクのスクロール実装、インデント修正

--- js/scroll.js
+++ js/scroll.js
@@ -1,20 +1,29 @@
 $(function() {
-    var TopBtn = $('#PageTopBtn');    
-    TopBtn.hide();
-    //スクロール位置が100でボタンを表示
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 3000) {
-            TopBtn.slideDown();
-        } 
-        else {
-            TopBtn.slideUp();
-        }
-    });
-    //ボタンを押下するとトップへ移動
-    TopBtn.click(function() {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 300);
-        return false;
-    });
+  // 右下の上部へ戻るボタン
+  var TopBtn = $('#PageTopBtn');    
+  TopBtn.hide();
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+      TopBtn.slideDown();
+    } 
+    else {
+      TopBtn.slideUp();
+    }
+  });
+  TopBtn.click(function() {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300);
+    return false;
+  });
+
+  // ページ内リンク
+  $('a[href^="#"]').click(function() {
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
 });
